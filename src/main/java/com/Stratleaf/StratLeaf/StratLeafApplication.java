ef83d7b69a98ca74c99d741bc8275ca48c44d43a
Bastian <bastian.polewka@gmx.de>
2025-10-29 17:52:27 +0100

    Springboot Init

---
+++ src/main/java/com/Stratleaf/StratLeaf/StratLeafApplication.java
@@ -0,0 +1,13 @@
+package com.Stratleaf.StratLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StratLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StratLeafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Stratleaf/StratLeaf/StratLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stratleaf.StratLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StratLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
